1a50e7defde11c1c5a1d2a9ee1d33fd653c30122
Marc Dacanay <mdacanay@gmail.com>
2019-08-12 21:00:05 +1000

    React ToDo - Initial Implementation

---
+++ react-todo/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <label>
+        <input type="checkbox" />
+        Check Me!
+        <br/>
+      </label>
+      <label>
+        <input type="checkbox" />
+        Check Me Too!
+        <br/>
+      </label>
+      <label>
+        <input type="checkbox" />
+        Don't Forget To Check Me As Well!
+        <br/>
+      </label>
+    </div>
+  );
+}
+
+export default App;
